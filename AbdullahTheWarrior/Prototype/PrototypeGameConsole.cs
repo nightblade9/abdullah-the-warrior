1717867b4266431cb26475e33d9f7e5e650ea8ce
nightblade9 <nightbladecodes@gmail.com>
2018-11-28 16:22:28 -0500

    Create monster class + creation monster Create data instance for player Refactor drawing code to be DRY and easier to consume

--- AbdullahTheWarrior/Prototype/PrototypeGameConsole.cs
+++ AbdullahTheWarrior/Prototype/PrototypeGameConsole.cs
@@ -9,6 +9,8 @@ namespace DeenGames.AbdullahTheWarrior.Prototype
     public class PrototypeGameConsole : SadConsole.Console
     {
         private readonly Color DarkGrey = Color.FromNonPremultiplied(64,64,64,255);
+        // Words cannot express my dismay. And amusement.
+        private Monster player = new Monster('@', Color.White, 40, 5, 3);
 
         // TODO: use an entity with a position component
         private int playerX = 0;
@@ -58,36 +60,70 @@ namespace DeenGames.AbdullahTheWarrior.Prototype
                 {
                     if (x == 0 || y == 0 || x == this.Width - 1 || y == this.Height - 1)
                     {
-                        // TODO: we should probably cache Cell instances, I'm sure this will hit the GC hard.
-                        this.SetCellAppearance(x, y, new Cell() { Background = Color.Black, Foreground = Color.Gray });
-                        this.SetGlyph(x, y, '#');
+                        this.DrawCharacter(x, y, '#', Color.Gray);
                     }
                     else if (x >= 35 && x <= 45 && y >= 15 && y <= 20)
                     {
-                        this.SetCellAppearance(x, y, new Cell() { Background = Color.Black, Foreground = Color.Gray });
-                        this.SetGlyph(x, y, '#');
+                        this.DrawCharacter(x, y, '#', Color.Gray);
                     }
                     else
                     {
-                        this.SetCellAppearance(x, y, new Cell() { Background = Color.Black, Foreground = DarkGrey });
-                        this.SetGlyph(x, y, '.');
+                        this.DrawCharacter(x, y, '.', DarkGrey);
                     }
                 }
             }
 
-            this.SetCellAppearance(playerX, playerY, new Cell() { Background = Color.Black, Foreground = Color.White });
-            this.SetGlyph(playerX, playerY, '@');
+            this.DrawCharacter(playerX, playerY, player.Character, player.Color);
         }
 
         private void GenerateMonsters()
         {
-            //throw new NotImplementedException();
         }
 
         private bool IsWalkable(int x, int y)
         {
             // Poor man's data structures: visual representation *is* our data.
             return this.GetGlyph(x, y) == '.';
+        }
+
+        private void DrawCharacter(int x, int y, char character, Color color)
+        {
+            // TODO: we should probably cache Cell instances, I'm sure this will hit the GC hard.
+            this.SetCellAppearance(x, y, new Cell() { Background = Color.Black, Foreground = color });
+            this.SetGlyph(x, y, character);
+        }
+
+        private class Monster
+        {
+            public int CurrentHealth { get; }
+            public int TotalHealth { get; }
+            public int Strength { get; }
+            public int Defense { get; }
+            public Color Color { get; }
+            public char Character { get; }
+
+            // This code makes me cry.
+            public static Monster Create(string name)
+            {
+                if (name == "Brigand")
+                {
+                    return new Monster('b', Color.Red, 20, 4, 1);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Not sure how to create a {name} monster");
+                }
+            }
+            
+            public Monster(char character, Color color, int health, int strength, int defense)
+            {
+                this.Character = character;
+                this.Color = color;
+                this.CurrentHealth = health;
+                this.TotalHealth = health;
+                this.Strength = strength;
+                this.Defense = defense;
+            }
         }
     }
 }

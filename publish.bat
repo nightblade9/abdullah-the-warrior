44950c8ecf10445955c5904e087239bf4c6601a9
nightblade9 <nightbladecodes@gmail.com>
2018-11-29 13:13:23 -0500

    Add ILLink.Tasks to reduce published size by 40% (70mb => 40mb)

--- publish.bat
+++ publish.bat
@@ -1,7 +1,7 @@
 cd AbdullahTheWarrior
 
 REM Source: https://stackoverflow.com/questions/44074121/build-net-core-console-application-to-output-an-exe
-dotnet publish -c Release -r win10-x64
+dotnet publish -c Release -r win10-x64 -o out_windows
 
 REM for Linux builds!
-dotnet publish -c Release -r ubuntu.16.10-x64
+REM dotnet publish -c Release -r ubuntu.16.10-x64 -o out_linux
